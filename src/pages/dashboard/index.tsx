281519908fd7e1f44a2dbd27171887012dea448e
DerivFX4 <derivfx4@gmail.com>
2026-08-23 20:12:18 +0300

    feat: mount floating risk disclaimer on dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../hooks/useStore';
+import RiskDisclaimer from './risk-disclaimer';
 
 const Dashboard = observer(() => {
     const { ui } = useStore();
@@ -8,6 +9,7 @@ const Dashboard = observer(() => {
         <div style={{ paddingTop: '12px' }}>
             <div>Dashboard observing from mobx store.</div>
             <div>isAuthorized: {isAuthorized.toString()}</div>
+            <RiskDisclaimer />
         </div>
     );
 });
