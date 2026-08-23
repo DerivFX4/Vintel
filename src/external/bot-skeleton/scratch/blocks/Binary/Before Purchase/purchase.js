622732decba2873da9b4bf3adf12fe24b954d052
DerivFX4 <derivfx4@gmail.com>
2026-08-23 23:59:03 +0300

    Support paired Both purchases for Only Ups and Only Downs

--- src/external/bot-skeleton/scratch/blocks/Binary/Before Purchase/purchase.js
+++ src/external/bot-skeleton/scratch/blocks/Binary/Before Purchase/purchase.js
@@ -5,20 +5,12 @@ import { excludeOptionFromContextMenu, modifyContextMenu } from '../../../utils'
 window.Blockly.Blocks.purchase = {
     init() {
         this.jsonInit(this.definition());
-
-        // Ensure one of this type per statement-stack
         this.setNextStatement(false);
     },
     definition() {
         return {
             message0: localize('Purchase {{ contract_type }}', { contract_type: '%1' }),
-            args0: [
-                {
-                    type: 'field_dropdown',
-                    name: 'PURCHASE_LIST',
-                    options: [['', '']],
-                },
-            ],
+            args0: [{ type: 'field_dropdown', name: 'PURCHASE_LIST', options: [['', '']] }],
             previousStatement: null,
             colour: window.Blockly.Colours.Special1.colour,
             colourSecondary: window.Blockly.Colours.Special1.colourSecondary,
@@ -30,50 +22,46 @@ window.Blockly.Blocks.purchase = {
     meta() {
         return {
             display_name: localize('Purchase'),
-            description: localize(
-                'Use this block to purchase the specific contract you want. You may add multiple Purchase blocks together with conditional blocks to define your purchase conditions. This block can only be used within the Purchase conditions block.'
-            ),
+            description: localize('Use this block to purchase the specific contract you want. You may add multiple Purchase blocks together with conditional blocks to define your purchase conditions. This block can only be used within the Purchase conditions block.'),
             key_words: localize('buy'),
         };
     },
     onchange(event) {
-        if (!this.workspace || window.Blockly.derivWorkspace.isFlyoutVisible || this.workspace.isDragging()) {
-            return;
-        }
-
+        if (!this.workspace || window.Blockly.derivWorkspace.isFlyoutVisible || this.workspace.isDragging()) return;
         if (event.type === window.Blockly.Events.BLOCK_CREATE && event.ids.includes(this.id)) {
             this.populatePurchaseList(event);
         } else if (event.type === window.Blockly.Events.BLOCK_CHANGE) {
-            if (event.name === 'TYPE_LIST' || event.name === 'TRADETYPE_LIST') {
-                this.populatePurchaseList(event);
-            }
+            if (event.name === 'TYPE_LIST' || event.name === 'TRADETYPE_LIST') this.populatePurchaseList(event);
         } else if (event.type === window.Blockly.Events.BLOCK_DRAG && !event.isStart && event.blockId === this.id) {
             const purchase_type_list = this.getField('PURCHASE_LIST');
-            const purchase_options = purchase_type_list.menuGenerator_; // eslint-disable-line
-
-            if (purchase_options[0][0] === '') {
-                this.populatePurchaseList(event);
-            }
+            const purchase_options = purchase_type_list.menuGenerator_;
+            if (purchase_options[0][0] === '') this.populatePurchaseList(event);
         }
     },
     populatePurchaseList(event) {
         const trade_definition_block = this.workspace.getTradeDefinitionBlock();
+        if (!trade_definition_block) return;
+        const trade_type_block = trade_definition_block.getChildByType('trade_definition_tradetype');
+        const contract_type_block = trade_definition_block.getChildByType('trade_definition_contracttype');
+        if (!trade_type_block || !contract_type_block) return;
 
-        if (trade_definition_block) {
-            const trade_type_block = trade_definition_block.getChildByType('trade_definition_tradetype');
-            const trade_type = trade_type_block.getFieldValue('TRADETYPE_LIST');
-            const contract_type_block = trade_definition_block.getChildByType('trade_definition_contracttype');
-            const contract_type = contract_type_block.getFieldValue('TYPE_LIST');
-            const purchase_type_list = this.getField('PURCHASE_LIST');
-            const purchase_type = purchase_type_list.getValue();
-            const contract_type_options = getContractTypeOptions(contract_type, trade_type);
+        const trade_type = trade_type_block.getFieldValue('TRADETYPE_LIST');
+        const contract_type = contract_type_block.getFieldValue('TYPE_LIST');
+        const purchase_type_list = this.getField('PURCHASE_LIST');
+        const purchase_type = purchase_type_list.getValue();
+        const contract_type_options = getContractTypeOptions(contract_type, trade_type);
 
-            purchase_type_list.updateOptions(contract_type_options, {
-                default_value: purchase_type,
-                event_group: event.group,
-                should_pretend_empty: true,
-            });
+        // Vintel extension: when Contract Type is Both, allow one valid purchase
+        // block to submit both configured contracts as a pair.
+        if (contract_type === 'both' && contract_type_options.length > 1) {
+            contract_type_options.unshift([localize('Both'), 'both']);
         }
+
+        purchase_type_list.updateOptions(contract_type_options, {
+            default_value: purchase_type,
+            event_group: event.group,
+            should_pretend_empty: true,
+        });
     },
     customContextMenu(menu) {
         const menu_items = [localize('Enable Block'), localize('Disable Block')];
@@ -85,7 +73,8 @@ window.Blockly.Blocks.purchase = {
 
 window.Blockly.JavaScript.javascriptGenerator.forBlock.purchase = block => {
     const purchaseList = block.getFieldValue('PURCHASE_LIST');
-
-    const code = `Bot.purchase('${purchaseList}');\n`;
-    return code;
+    if (purchaseList === 'both') {
+        return "Bot.purchase('RUNHIGH');\nBot.purchase('RUNLOW');\n";
+    }
+    return `Bot.purchase('${purchaseList}');\n`;
 };
